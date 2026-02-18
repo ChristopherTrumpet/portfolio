db810d0fbb4b9e75e5d85f0f863dc23b0ffa3a4b
Christopher Mark Trumpet Jr <ctrumpet@purdue.edu>
2026-02-18 02:41:07 -0500

    added back to top button and improved image loads for media archive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,32 @@
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  output: 'static',
+
+  output: "static",
+
   env: {
     schema: {
       // Use 'public' if client-side
       // Use 'server' if frontmatter/SSR
       API_URL: envField.string({
-        context: 'server', 
-        access: 'public', 
-        default: 'https://api.cmkt.dev'
+        context: "server",
+        access: "public",
+        default: "https://api.cmkt.dev",
       }),
-    }
+    },
   },
+
   integrations: [react(), icon()],
   adapter: vercel(),
 });
